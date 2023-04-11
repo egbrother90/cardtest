702ed4d580ba010c2e0b771510dbe15878094d34
[헬스케어] 이지형 <egbrother@douzone.com>
2023-04-11 13:23:49 +0900

    d

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const custom = [
+    {
+      key: 1,
+      bgcolor: 'orange',
+      company: '(주)더존비즈온',
+      name: '김채원',
+      text3: 'text3'
+    },
+    {
+      key: 2,
+      bgcolor: 'blue',
+      text1: 'text0',
+      text2: 'text0',
+      text3: 'text0'
+    },
+    {
+      key: 3,
+      bgcolor: 'yellow',
+      text1: 'text7',
+      text2: 'text8',
+      text3: 'text9'
+    }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="test-wrap custorm">
+        {custom.map(custom => {
+          return(
+            <div
+              style={{backgroundColor: custom.bgcolor}}
+              key={custom.key}
+            >
+              <ul>
+                <li>{custom.text1}</li>
+                <li>{custom.text2}</li>
+                <li>{custom.text3}</li>
+              </ul>
+            </div>
+          )
+        })}
+      </div>
+    </>
   );
 }
 
