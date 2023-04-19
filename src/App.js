94d99831ac3f451deede1ab7b11a37b16592e943
[헬스케어] 이지형 <egbrother@douzone.com>
2023-04-19 18:00:48 +0900

    d

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import './App.css';
-import { CardList } from './components/CardList';
+import { Card, CardList } from './components/CardList';
+import './components/CardList/CardList.scss';
 
 function App() {
   const cards = [
     {
       id: 1,
-      title: 'card1',
+      title: 'Card1',
       description: 'This is card 1.'
     },
     {
       id: 2,
-      title: 'card1',
-      description: 'This is card 1.'
+      title: 'Card2',
+      description: 'This is card 2.'
     },
     {
       id: 3,
-      title: 'card1',
-      description: 'This is card 1.'
+      title: 'Card3',
+      description: 'This is card 3.'
     },
     {
       id: 4,
-      title: 'card1',
-      description: 'This is card 1.'
+      title: 'Card4',
+      description: 'This is card 4.'
     },
   ]
 
@@ -29,7 +30,10 @@ function App() {
     <>
       <CardList
         cards={cards}
-      />
+        padding='100'
+      >
+        <Card />
+      </CardList>
     </>
   );
 }
--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -1,13 +1,35 @@
 import React from "react";
 import Card from "./Card";
+import iconEmpty from '../CardList/icon-empty.svg';
 
-const cardlist = ({ cards }) => {
+const cardlist = ({ cards, padding }) => {
+
+  const styleListPadding = padding ? `${padding}` : "";
+  // const styleOption = [stylePadding].join(' ').trimEnd();
+
+
   return(
-    <div className="card-list">
-      {cards.map(card => (
-        <Card Key={card.id} card={card}></Card>
-      ))}
-    </div>
+    <>
+      {
+        cards ?
+          <div className="card-list" style={{padding: `${styleListPadding}px`}}>
+            {cards.map(card => (
+              <Card
+                key={card.id}
+                card={card}
+              >
+              </Card>
+            ))}
+          </div>
+        :
+          <div className="empty">
+            <div className="empty-inner">
+                <img src={iconEmpty} alt="데이터가 존재하지 않습니다." />
+                <p>데이터가 존재하지 않습니다.</p>
+            </div>
+          </div>
+      }
+    </>
   )
 }
 
